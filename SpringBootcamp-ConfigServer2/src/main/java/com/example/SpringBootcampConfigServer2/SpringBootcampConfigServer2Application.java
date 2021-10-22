3cd2b77521d810566de86d51841c8e9100b67f09
jesusmiguelgarciaduran <jesusmiguelgarciaduran@gmail.com>
2021-10-22 10:06:45 +0200

    .

---
+++ SpringBootcamp-ConfigServer2/src/main/java/com/example/SpringBootcampConfigServer2/SpringBootcampConfigServer2Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootcampConfigServer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootcampConfigServer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootcampConfigServer2Application.class, args);
+	}
+
+}
---
+++ SpringBootcamp-ConfigServer2/src/test/java/com/example/SpringBootcampConfigServer2/SpringBootcampConfigServer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootcampConfigServer2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootcampConfigServer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
